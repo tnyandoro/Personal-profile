6fb1ddff794b0d3c332d20aa3ef3dd1bf7786610
Tendai Nyandoro <tendai1@hotmail.com>
2021-04-18 16:20:59 +0200

    Configure the css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
-module.export = {
-  mode: 'development',
-  entry
-}
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
